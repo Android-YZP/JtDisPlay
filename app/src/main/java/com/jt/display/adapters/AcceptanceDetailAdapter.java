7e2cc1ce843f8e5dd98d9bb630c3291bf9765f2a
Smith <724784530@qq.com>
2020-08-26 10:47:42 +0800

    电视界面尺寸适配

--- app/src/main/java/com/jt/display/adapters/AcceptanceDetailAdapter.java
+++ app/src/main/java/com/jt/display/adapters/AcceptanceDetailAdapter.java
@@ -28,7 +28,7 @@ public class AcceptanceDetailAdapter extends ListBaseAdapter<String> {
     public void onBindItemHolder(SuperViewHolder holder, final int position) {
 //        View rootView = holder.getView(R.id.ll_root_view);
         TextView tvCode = holder.getView(R.id.tv);
-        tvCode.setText(mDataList.get(position));
+//        tvCode.setText(mDataList.get(position));
     }
 
 }
